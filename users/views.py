02db87bd630a832b1ac7444e016b3bffdd9441f3
Malindu Liyanage <136006504+MalinduLiyanage@users.noreply.github.com>
2024-05-19 12:02:30 +0530

    Update views.py -2

--- users/views.py
+++ users/views.py
@@ -61,19 +61,11 @@ def activate_user(request, token):
 
 
 @api_view(['GET'])
-def login_user(request, email, password):
-    # Authenticate user based on email and password
-    user = authenticate(request, username=email, password=password)
-    
-    if user is not None:
-        # Check if the user's activation status is false
-        if not user.profile.activation_status:  # Assuming activation_status is in a related profile model
-            return Response({'error': 'Account not activated'}, status=status.HTTP_403_FORBIDDEN)
-        
-        # User is authenticated and activated
+def login_user(request, email,password,activation_status):
+    try:
+        user = User.objects.get(email=email,password=password,activation_status=activation_status)
         return Response({'message': 'OK'}, status=status.HTTP_200_OK)
-    else:
-        # User credentials do not match
-        return Response({'error': 'Invalid user'}, status=status.HTTP_404_NOT_FOUND)
+    except User.DoesNotExist:
+        return Response({'error': 'NOOOOO'}, status=status.HTTP_404_NOT_FOUND)
 
 
